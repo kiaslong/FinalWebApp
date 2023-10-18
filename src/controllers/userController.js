8c3588ffe1605d365efda4cd075e8cdfeaf1956f
kiaslong <kiraslong@gmail.com>
2023-10-19 00:57:35 +0700

    chinh register va login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,9 +3,11 @@ const userService = require('../services/userService'); // Import the user servi
 // Register a new user
 exports.registerUser = async (req, res) => {
   try {
-    const { email, password, fullName } = req.body;
- 
-    const result = await userService.registerUser({ email, password, fullName });
+    const  email = req.body.email;
+    let temp = email.split('@')
+    username=temp[0];
+    password = username;
+    const result = await userService.registerUser({ email, password, username });
     res.status(201).json(result);
   } catch (error) {
     console.error('Error registering user:', error);
@@ -16,9 +18,10 @@ exports.registerUser = async (req, res) => {
 // Login user
 exports.loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const result = await userService.loginUser({ email, password });
-    res.status(200).json(result);
+    const { username, password } = req.body;
+    console.log(username, password);
+    const result = await userService.loginUser({ username, password });
+    return res.redirect('/');
   } catch (error) {
     console.error('Error logging in:', error);
     res.status(500).json({ message: 'Internal server error' });
